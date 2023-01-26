b72ee5db06dd3bac7cb966da0eef2f1642cb65e5
Yoni Lavi <yoni@lavi.fm>
2023-01-26 12:22:49 +0000

    Refactored to a generic function
    Credit to @lutzky

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,34 +2,14 @@ use fixed_heap::FixedHeap;
 
 fn main() {
     let input = include_str!("my_input.txt");
-
-    println!("Part 1 (top elf): {}", part1(input));
-    println!("Part 2 (sum of 3 top elves): {}", part2(input));
+    println!("Part 1 (top elf): {}", top_n_elves::<1>(input));
+    println!("Part 2 (sum of 3 top elves): {}", top_n_elves::<3>(input));
 }
 
-fn part1(input: &str) -> u32 {
+fn top_n_elves<const N: usize>(input: &str) -> u32 {
     let mut elf_sum = 0;
-    let mut max_sum = 0;
-    for line in input.lines() {
-        match line.parse::<u32>() {
-            Ok(n) => {
-                elf_sum += n;
-            },
-            Err(_) => { // The only error we expect is a blank line
-                if elf_sum > max_sum {
-                    max_sum = elf_sum;
-                }
-                elf_sum = 0;
-            }
-        }
-    }
-    max_sum
-}
-
-fn part2(input: &str) -> u32 {
-    let mut elf_sum = 0;
-    let mut top_3_elves: FixedHeap<u32, 3> = FixedHeap::default();
-    let heap_comparer = |a: &u32, b: &u32, _: &()| a > b;
+    let mut top_elves: FixedHeap<u32, N> = FixedHeap::default();
+    let comparer = |a: &u32, b: &u32, _: &()| a > b;
 
     for line in input.lines() {
         match line.parse::<u32>() {
@@ -37,10 +17,10 @@ fn part2(input: &str) -> u32 {
                 elf_sum += n;
             },
             Err(_) => { // Assume blank line and finish processing this elf
-                top_3_elves.push(elf_sum, &heap_comparer, &());
+                top_elves.push(elf_sum, &comparer, &());
                 elf_sum = 0;
             }
         }
     }
-    top_3_elves.iter().sum()
+    top_elves.iter().sum()
 }
