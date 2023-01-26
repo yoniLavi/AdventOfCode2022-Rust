c3547a527f5ecc2100470fc29cc29ec04a4ab622
Yoni Lavi <yoni@lavi.fm>
2023-01-26 00:59:38 +0000

    switched to fixed_heap

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,12 +1,35 @@
+use fixed_heap::FixedHeap;
+
 fn main() {
     let input = include_str!("my_input.txt");
-    println!("Part 1 (top elf): {}", process_elves(input, 1));
-    println!("Part 2 (sum of 3 top elves): {}", process_elves(input, 3));
+
+    println!("Part 1 (top elf): {}", part1(input));
+    println!("Part 2 (sum of 3 top elves): {}", part2(input));
 }
 
-fn process_elves(input: &str, top_n: usize) ->u32 {
-    let mut elf_sum = 0; // Sum of calories for current elf
-    let mut top_elves = vec![0; top_n]; // Calories for the top `top_n` elves
+fn part1(input: &str) -> u32 {
+    let mut elf_sum = 0;
+    let mut max_sum = 0;
+    for line in input.lines() {
+        match line.parse::<u32>() {
+            Ok(n) => {
+                elf_sum += n;
+            },
+            Err(_) => { // The only error we expect is a blank line
+                if elf_sum > max_sum {
+                    max_sum = elf_sum;
+                }
+                elf_sum = 0;
+            }
+        }
+    }
+    max_sum
+}
+
+fn part2(input: &str) -> u32 {
+    let mut elf_sum = 0;
+    let mut top_3_elves: FixedHeap<u32, 3> = FixedHeap::default();
+    let heap_comparer = |a: &u32, b: &u32, _: &()| a > b;
 
     for line in input.lines() {
         match line.parse::<u32>() {
@@ -14,12 +37,10 @@ fn process_elves(input: &str, top_n: usize) ->u32 {
                 elf_sum += n;
             },
             Err(_) => { // Assume blank line and finish processing this elf
-                top_elves.push(elf_sum);
-                top_elves.sort();
-                top_elves = (top_elves[1..(top_n+1)]).to_vec();
+                top_3_elves.push(elf_sum, &heap_comparer, &());
                 elf_sum = 0;
             }
         }
     }
-    return top_elves.iter().sum();
+    top_3_elves.iter().sum()
 }
