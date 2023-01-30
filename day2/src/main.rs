71aeed16071e28a64a7afa96606a638ef5f05f97
Yoni Lavi <yoni@lavi.fm>
2023-01-30 10:01:03 +0000

    day2 - made operand order consistent

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -15,9 +15,9 @@ fn part1(round: &str) -> u32 {
 
 fn part2(round: &str) -> u32 {
     match round {
-        "A X" => 3+0, "A Y" => 1+3, "A Z" => 2+6,
-        "B X" => 1+0, "B Y" => 2+3, "B Z" => 3+6,
-        "C X" => 2+0, "C Y" => 3+3, "C Z" => 1+6,
+        "A X" => 0+3, "A Y" => 3+1, "A Z" => 6+2,
+        "B X" => 0+1, "B Y" => 3+2, "B Z" => 6+3,
+        "C X" => 0+2, "C Y" => 3+3, "C Z" => 6+1,
         _ => 0 // Should never happen
     }
 }
